4aa82c0ff2b2129d15607a961d5172c9a197d356
Yasir Romaya <yasir.romaya@gmail.com>
2022-06-11 14:04:42 +0300

    Release 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'UI/Home.dart';
+import 'ui/Home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Yasir Romaya',
       debugShowCheckedModeBanner: false,
       home: Home(),
