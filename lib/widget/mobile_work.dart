774f67aecb232eb0886902e3761bf9e5d02a06ca
Yasir Romaya <yasir.romaya@gmail.com>
2022-06-25 00:23:05 +0300

    release 1.2.1

--- lib/widget/mobile_work.dart
+++ lib/widget/mobile_work.dart
@@ -16,66 +16,11 @@ class _MobileWorkState extends State<MobileWork> {
     return ConstrainedBox(
       constraints: BoxConstraints.tightFor(
         width: size.width,
-        height: size.height * 1.4,
+        height: size.height * 1.8,
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: size.height * 0.07,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: SizedBox(
-                    height: size.height * 1.2,
-                    //color: Colors.indigo,
-                    child: Stack(
-                      children: [
-                        const Center(
-                          child: VerticalDivider(
-                            color: Color(0xff64FFDA),
-                            thickness: 1.75,
-                            width: 10,
-                            indent: 10,
-                            endIndent: 10,
-                          ),
-                        ),
-                        Center(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: const [
-                            CircleAvatar(
-                              backgroundColor: Color(0xff64FFDA),
-                              child: SizedBox(),
-                            ),
-                            CircleAvatar(
-                              backgroundColor: Color(0xff64FFDA),
-                              child: SizedBox(),
-                            ),
-                            CircleAvatar(
-                              backgroundColor: Color(0xff64FFDA),
-                              child: SizedBox(),
-                            ),
-                            CircleAvatar(
-                              backgroundColor: Color(0xff64FFDA),
-                              child: SizedBox(),
-                            ),
-                          ],
-                        ))
-                      ],
-                    )),
-              ),
-              Expanded(
-                flex: 4,
-                child: SizedBox(
-                  height: size.height * 1.2,
-                  child: const MobileWorkBox(),
-                ),
-              )
-            ],
-          )
-        ],
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: MobileWorkBox(),
       ),
     );
   }
